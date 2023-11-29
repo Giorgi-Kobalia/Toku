b0b40d652726fea1500de50839df77727f316b28
Giorgi Kobalia <145305543+NopperaBoo@users.noreply.github.com>
2023-11-30 00:25:12 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,78 @@
 var swiper = new Swiper(".mySwiper", {
   effect: "coverflow",
 
-  loop: true,
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "5",
+  breakpoints: {
 
-  coverflowEffect: {
-    rotate: 0,
-    stretch: -20,
-    depth: 100,
-    modifier: 1,
-    scale: 0.7,
-  },
+    200: {
+      loop: true,
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "1",
 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+      coverflowEffect: {
+        rotate: 0,
+        stretch: -10,
+        depth: 50,
+        modifier: 1,
+        scale: 0.85 ,
+      },
 
-  autoplay: {
-    delay: 5000,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+
+      autoplay: {
+        delay: 5000,
+      },
+    },
+
+    600: {
+      loop: true,
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "3",
+
+      coverflowEffect: {
+        rotate: 0,
+        stretch: -10,
+        depth: 50,
+        modifier: 1,
+        scale: 0.85 ,
+      },
+
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+
+      autoplay: {
+        delay: 5000,
+      },
+    },
+
+    960: {
+      loop: true,
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "5",
+
+      coverflowEffect: {
+        rotate: 0,
+        stretch: -10,
+        depth: 50,
+        modifier: 1,
+        scale: 0.85 ,
+      },
+
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+
+      autoplay: {
+        delay: 5000,
+      },
+    },
   },
 });
