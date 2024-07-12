999a57e9864fd768e90a718f5deea55885a5f983
gkobalia <gkobalia@crocobet.com>
2024-07-12 16:46:54 +0400

    C

--- script.js
+++ script.js
@@ -117,3 +117,45 @@ const scrollVisibility = () => {
 window.addEventListener("scroll", scrollVisibility);
 
 scrollVisibility();
+
+
+
+// wadawdwa
+
+
+const chakrasContainer = document.querySelector('.all-chakras');
+const chakraElements = document.querySelectorAll('.card');
+
+const chakraColors = {
+  muladhara: '#eb040c',
+  svadhistana: '#f57200',
+  manipura: '#feb302',
+  anahata: '#02a552',
+  vishudha: '#00adef',
+  ajna: '#2e47a4',
+  sahasrara: '#a4108c'
+};
+
+function updateScrollbarColor() {
+  let middleScrollPosition = chakrasContainer.scrollLeft + chakrasContainer.clientWidth / 2;
+
+  chakraElements.forEach((chakra) => {
+    let chakraLeft = chakra.offsetLeft;
+    let chakraRight = chakraLeft + chakra.clientWidth;
+
+    if (middleScrollPosition >= chakraLeft && middleScrollPosition <= chakraRight) {
+      let chakraClass = Array.from(chakra.classList).find(cls => chakraColors[cls]);
+      console.log(chakraClass);
+      if (chakraClass) {
+        document.documentElement.style.setProperty('--scrollbar-thumb-color', chakraColors[chakraClass]);
+      }
+    }
+  });
+}
+
+chakrasContainer.addEventListener('scroll', updateScrollbarColor);
+
+window.onload = function() {
+  chakrasContainer.scrollLeft = (chakrasContainer.scrollWidth - chakrasContainer.clientWidth) / 2;
+  updateScrollbarColor();
+};
