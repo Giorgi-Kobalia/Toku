dbb554b64a6907cbab9de024ffb2e74c85381c77
gkobalia <gkobalia@crocobet.com>
2024-07-24 12:15:22 +0400

    C

--- index/crystals/crystals.js
+++ index/crystals/crystals.js
@@ -564,6 +564,51 @@ const crystals = [
         satauri: ` ემოციური კეთილდღეობა  `,
         aghwera: ` ეს ხელს უწყობს ემოციურ წონასწორობას, სტაბილურობას და კომფორტს. `,
       },
+
+
+
+
+      {
+        satauri: ` აღწერა `,
+        aghwera: ` ავენტურინი არის კვარცის ფორმა, რომელიც ხასიათდება მისი მწვანედან მოლურჯო-მომწვანო შეფერილობით და პაწაწინა მეტალის ჩანართების არსებობით, რომლებიც ქმნიან ცქრიალა ან მოციმციმე ეფექტს, რომელიც ცნობილია როგორც . ეს არის პოპულარული და მრავალმხრივი ძვირფასი ქვა, რომელიც გამოიყენება სხვადასხვა სახის სამკაულებსა და ჩუქურთმებში. `,
+      },
+      {
+        satauri: ` ჩაკრა `,
+        aghwera: ` ავენტურინი პირველ რიგში ასოცირდება გულის ჩაკრასთან (ანაჰატა). გულის ჩაკრა დაკავშირებულია სიყვარულთან, თანაგრძნობასთან და ემოციურ კეთილდღეობასთან. `,
+      },
+      {
+        satauri: ` მაგიური შესაძლებლობები  `,
+        aghwera: ` ავენტურინი გამოიყენება სხვადასხვა მაგიური და მეტაფიზიკური მიზნებისთვის, მათ შორის: `,
+      },
+      {
+        satauri: ` განკურნება და კომფორტი `,
+        aghwera: ` ითვლება, რომ ის ხელს უწყობს განკურნებას, კომფორტს და ემოციურ წონასწორობას. `,
+      },
+      {
+        satauri: ` კეთილდღეობა `,
+        aghwera: ` ავენტურინი ხშირად ასოცირდება სიუხვისა და კეთილდღეობის მოზიდვასთან. `,
+      },
+      {
+        satauri: ` ენერგიის ჰარმონიზაცია `,
+        aghwera: ` მას შეუძლია ენერგიის ჰარმონიზაცია და დაბალანსება.`,
+      },
+      {
+        satauri: ` კრეატიულობა `,
+        aghwera: `  ავენტურინი ასტიმულირებს შემოქმედებითობას და მხატვრულ გამოხატვას. `,
+      },
+      {
+        satauri: ` დაცვა `,
+        aghwera: `  ზოგი იყენებს მას უარყოფითი ენერგიებისგან დასაცავად. `,
+      },
+      {
+        satauri: ` ჯანმრთელობის გამოყენება `,
+        aghwera: ` ავენტურინი ასტიმულირებს შემოქმედებითობას და მხატვრულ გამოხატვას. `,
+      },
+      {
+        satauri: ` ემოციური კეთილდღეობა  `,
+        aghwera: ` ეს ხელს უწყობს ემოციურ წონასწორობას, სტაბილურობას და კომფორტს. `,
+      },
+
     ],
   },
 ];
@@ -588,19 +633,30 @@ more.forEach((more, index) => {
   more.addEventListener("click", () => {
     let html = "";
     crystals[index].desc.forEach((item) => {
-      html += `<div> <span style="font-weight: 600; font-size : 22px"> ${item.satauri} </span> :  ${item.aghwera}</div> <br>`;
+      html += `<div> <span style="font-weight: 600; font-size: 22px;"> ${item.satauri} </span>: ${item.aghwera}</div><br>`;
     });
 
     test.innerHTML = html;
-
     desc_desc.style.display = "flex";
-    allCrystals.style.display = "none";
+    allCrystals.style.opacity = '0'
+
+    document.body.style.overflow = "hidden";
+    document.body.addEventListener("touchmove", preventDefault, {
+      passive: false,
+    });
   });
 });
 
 const close = document.querySelector(".close");
 close.addEventListener("click", () => {
   test.innerHTML = "";
-  desc_desc.style = "display: none";
-  allCrystals.style = "display: grid";
+  desc_desc.style.display = "none";
+  allCrystals.style.opacity = '100'
+
+  document.body.style.overflow = "";
+  document.body.removeEventListener("touchmove", preventDefault);
 });
+
+function preventDefault(e) {
+  e.preventDefault();
+}
